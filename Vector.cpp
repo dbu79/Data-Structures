15d5a53e396f168919e20e123ca16d573d163b37
dbu79 <danielbutler245@gmail.com>
2026-08-24 18:41:53 +1200

    Finish rule of 5

--- Vector.cpp
+++ Vector.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iterator>
 #include <new>
 
 template <typename T> 
@@ -38,12 +39,55 @@ class Vector{
         // Copy Constructor
         Vector(const Vector& other)
             : size(other.size), capacity(other.capacity){
+                std::cout << "Copy constructor called" << std::endl;
                 data = static_cast<T*>(operator new(capacity * sizeof(T)));
                 for (size_t i{}; i < size;  i++){
                     new (data + i) T(other.data[i]);
                 }
             }
-            
+
+        // Copy Assignment
+        Vector& operator = (const Vector& other) {
+            std::cout << "Copy assignment called" << std::endl;
+            if (this != &other){
+                T* new_data = static_cast<T*>(operator new(other.capacity * sizeof(T)));
+                for (size_t i = 0; i < other.size; i++){
+                    new (new_data + i) T(other.data[i]);
+                }
+                for (size_t i = 0; i < size; i++){
+                    data[i].~T();
+                }
+                operator delete(data);
+                data = new_data;
+                size = other.size;
+                capacity = other.capacity;
+            }
+            return *this;
+        }
+
+        // Move Constructor
+        Vector(Vector&& other) noexcept : data(other.data), size(other.size), capacity(other.capacity) {
+            std::cout << "Move constructor called" << std::endl;
+            other.data = nullptr;
+            other.size = 0;
+            other.capacity = 0;
+        }
+        
+        // Move Assignment 
+        Vector& operator=(Vector&& other) noexcept {
+            std::cout << "Move assignment called" << std::endl;
+            if (this != &other){
+                for (size_t i{}; i < size; i++){
+                    data[i].~T();
+                }
+                operator delete(data);
+
+                data = other.data;
+                size = other.size;
+                capacity = other.capacity;
+            }
+            return *this;
+        }
         void PushBack(const T& value){
             if(size >= capacity){
                 Alloc(capacity == 0 ? 2 : capacity + capacity / 2);
@@ -86,12 +130,22 @@ class Vector{
 template <typename T>
 void PrintVec(const Vector<T>& vector){
     for(size_t i = 0; i < vector.Size(); i++){
-        std::cout << vector[i] << std::endl;
+        std::cout << vector[i] << " ";
     }
+    std::cout << std::endl;
 }
 
 int main() {
     Vector<int> vector;
-    // std::cout << vector.Size() << std::endl;
-    // std::cout << vector.Capacity();
+    vector.PushBack(2);
+    vector.PushBack(1);
+    PrintVec(vector);
+    
+    Vector<int> vector3;
+    vector3.PushBack(1);
+    vector3.PushBack(2);
+    auto a = vector;
+    a = vector;
+    Vector<int> vector2 = std::move(vector);
+    vector3 = std::move(vector);
 }
