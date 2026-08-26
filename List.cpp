f3779cc3d7b34fa6cbe06688c0221d2b8b45b78f
dbu79 <danielbutler245@gmail.com>
2026-08-26 17:57:00 +1200

    Add initial linked list class

---
+++ List.cpp
@@ -0,0 +1,69 @@
+#include <cstddef>
+#include <iostream>
+
+template <typename T> class LinkedList {
+private:
+  struct Node {
+    T data;
+    Node *next = nullptr;
+    Node *prev = nullptr;
+
+    Node(const T &value) : data(value) {};
+  };
+
+  Node *head = nullptr;
+  Node *tail = nullptr;
+  size_t size = 0;
+
+public:
+  LinkedList() {};
+  ~LinkedList() {};
+
+  void PushBack(const T &value) {
+    Node *newNode = new Node(value);
+
+    if (tail == nullptr) {
+      head = tail = newNode;
+    } else {
+      newNode->prev = tail;
+      tail->next = newNode;
+      tail = newNode;
+    }
+    size++;
+  }
+
+  void PushFront(const T &value) {
+    Node *newNode = new Node(value);
+
+    Node *current = tail;
+    if (head == nullptr) {
+      head = tail = newNode;
+    } else {
+      newNode->next = head;
+      head->prev = newNode;
+      head = newNode;
+    }
+    size++;
+  }
+
+  size_t Size() const { return size; }
+
+  void PrintList() {
+    Node *current = head;
+    while (current->next != nullptr) {
+      std::cout << current->data << " ";
+      current = current->next;
+    }
+  }
+};
+
+int main() {
+  LinkedList<int> list;
+
+  list.PushBack(1);
+  list.PushBack(2);
+  list.PushBack(3);
+
+  list.PrintList();
+  return 0;
+}
